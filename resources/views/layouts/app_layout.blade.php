9c8c83995e7080b0fa1fcecb792d5e9e9b1144ad
Raheel Rashid <raheel675403@gmail.com>
2025-04-22 21:00:58 +0500

    working

--- resources/views/layouts/app_layout.blade.php
+++ resources/views/layouts/app_layout.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
     <title>Dashboard</title>
     <!--begin::Primary Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
